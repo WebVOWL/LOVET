872f97aa93fbba23ad390f1986f7fbccc6984ad8
magz <11150183+TheMagzuz@users.noreply.github.com>
2026-04-13 13:23:44 +0200

    cleanup database regularly

--- crates/database/src/store.rs
+++ crates/database/src/store.rs
@@ -21,7 +21,7 @@ use vowlgrapher_parser::parser_util::{
 use vowlgrapher_serializer::prelude::GraphDisplayDataSolutionSerializer;
 use vowlgrapher_util::prelude::{DataType, ErrorRecord, VOWLGRAPHER_ENVIRONMENT, VOWLGrapherError};
 
-static GLOBAL_STORE: std::sync::OnceLock<Store> = std::sync::OnceLock::new();
+pub static GLOBAL_STORE: std::sync::LazyLock<Store> = std::sync::LazyLock::new(Store::default);
 
 /// The graph database.
 pub struct VOWLGrapherStore {
@@ -48,7 +48,7 @@ impl VOWLGrapherStore {
 
     /// Create a new database instance with a user id.
     pub fn new_for_user(user_id: String) -> Self {
-        let session = GLOBAL_STORE.get_or_init(Store::default).clone();
+        let session = GLOBAL_STORE.clone();
         let max_upload_size = VOWLGRAPHER_ENVIRONMENT.max_input_size_bytes.0;
         Self {
             session,
@@ -539,7 +539,7 @@ impl VOWLGrapherStore {
 
 impl Default for VOWLGrapherStore {
     fn default() -> Self {
-        let session = GLOBAL_STORE.get_or_init(Store::default).clone();
+        let session = GLOBAL_STORE.clone();
         Self::new(session)
     }
 }
