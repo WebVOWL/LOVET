71bad02817c95c629de6944c3a72012d77317a27
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-04-22 13:44:27 +0000

    Wip on metadata

--- src/events.rs
+++ src/events.rs
@@ -1,17 +1,28 @@
+use crate::errors::{ClientErrorKind, ErrorLogContext};
 use grapher::prelude::{EVENT_DISPATCHER, GUIEvent};
+use leptos::prelude::expect_context;
+use log::info;
 
 pub struct EventHandler;
 
 impl EventHandler {
-    #[expect(unused, reason = "pending implementation")]
-    pub fn handle_event() {
-        while let Ok(event) = EVENT_DISPATCHER.gui_read_chan.recv() {
-            match event {
-                GUIEvent::ShowMetadata(idx) => {
-                    // call relevant signal
+    pub async fn handle_event() {
+        loop {
+            match EVENT_DISPATCHER.gui_read_chan.recv().await {
+                Ok(event) => {
+                    match event {
+                        GUIEvent::ShowMetadata(idx) => {
+                            // call relevant signal
+                            info!("{idx}");
+                        }
+                        GUIEvent::HideMetadata() => {
+                            // call relevant signal
+                        }
+                    }
                 }
-                GUIEvent::HideMetadata() => {
-                    // call relevant signal
+                Err(e) => {
+                    let error_context = expect_context::<ErrorLogContext>();
+                    error_context.push(ClientErrorKind::EventHandlingError(e.to_string()).into());
                 }
             }
         }
